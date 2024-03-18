354e1f1071d8722758f045bc3de2a6944f24cb54
iamluhmrt <luizhenrique.business95@gmail.com>
2024-03-18 12:01:47 -0300

    convert project into pwa

--- front/src/main.js
+++ front/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 
 import 'semantic-ui-css/semantic.css'
 import router from './router'
+import './registerServiceWorker'
 new Vue({
     router,
     render: (h) => h(App),
